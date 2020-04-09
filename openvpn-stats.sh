fca81f725f5898f6bc3cc76e7aba488e8a9472af
jasonkinner <github@jasonkinner.com>
2020-04-09 14:48:01 -0400

    Updated comments on lines

--- openvpn-stats.sh
+++ openvpn-stats.sh
@@ -2,21 +2,21 @@
 
 input_file=/var/log/openvpn/openvpn-status.log
 
-declare -a data                                                 # create arrays to store data
-declare -a ip
+declare -a data                                                 # create array to store client data
+declare -a ip							# create array to store client private IPs
 i=0                                                             # keep a count of total lines
-j=0                                                             # secondary counter for private IP section
-while read line; do                                             # loop through all input lines
-    if [[ "$line" = *,* ]]; then                                # if line has a comma in it, it contains usable data
+j=0                                                             # secondary counter for private IP array
+while read line; do                                             # loop through all input lines from file
+    if [[ "$line" = *,* ]]; then                                # if line has a comma in it, it may contain usable data
         IFS=',' read -ra fields <<< "$line"                     # convert string to array, split by comma
-        if [[ "${fields[1]}" =~ \.[0-9]{1,3}:[0-9] ]]; then     # if 2nd field seems to contain an ip address
-                received=$((fields[2] / 2**20))                 # extract bytes received and convert to MB
-                sent=$((fields[3] / 2**20))                     # extract bytes sent and convertt to MB
+        if [[ "${fields[1]}" =~ \.[0-9]{1,3}:[0-9] ]]; then     # if 2nd field seems to contain an ip address:
+                received=$((fields[2] / 2**20))                 #   extract bytes received and convert to MB
+                sent=$((fields[3] / 2**20))                     #   extract bytes sent and convertt to MB
                 # store a long string of the relevant data with tab separators
                 data[$i]="${fields[0]}"$'\t'"${received}"$'\t'$'\t'"${sent}"$'\t'$'\t'"${fields[4]}"$'\t'
-                i=$((i+1))                                      # increment record counter to track total clients
+                i=$((i+1))                                      #   increment record counter to track total clients
         fi
-        if [[ "${fields[2]}" =~ \.[0-9]{1,3}:[0-9] ]]; then     # if 3rd field seems to contain an ip address
+        if [[ "${fields[2]}" =~ \.[0-9]{1,3}:[0-9] ]]; then     # if 3rd field seems to contain an ip address:
                 ip[$j]="${fields[0]}"                           # store the private ip info in another array
                 j=$((j+1))
         fi
@@ -28,5 +28,5 @@ echo "Client"$'\t'$'\t'$'\t'"MB Recvd"$'\t'"MB Sent"$'\t'$'\t'"Since"$'\t'$'\t'$
 
 for (( i=0; i<$j; i++ ))                # loop through the client records
 do
-        echo "${data[$i]}${ip[$i]}"     # output the client info and private ip
+        echo "${data[$i]}${ip[$i]}"     # output the client info, then private ip
 done
